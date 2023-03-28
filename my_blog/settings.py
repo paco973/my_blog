647b7bc8a4e379a660da84d3dd531ef0af5e0de0
pdjo <pdjo@laboratoire-arrow.com>
2023-03-28 16:56:47 +0200

    d

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
